9491cc06e32396a60a78a62cfb1c1fb7ef53d292
Darwis <darwisnarvaezdev@gmail.com>
2024-04-10 23:54:53 -0300

    Progress on menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 
-import { Button, Flex, Image, Tag } from '@chakra-ui/react';
+import { Button, Fade, Flex, Image, ScaleFade, Tag, useDisclosure } from '@chakra-ui/react';
 import { CenteredProps, SpaceInheritedFromParent } from './styles/chakra/Props.tsx';
 import Clouds from './components/Clouds.tsx';
 import Ground from './components/Ground.tsx';
@@ -11,6 +11,8 @@ import RushingClouds from './components/RushingClouds.tsx';
 import RocketWrapper from './components/RocketWrapper.tsx';
 
 function App(): React.ReactElement {
+
+  const { isOpen, onToggle } = useDisclosure()
 
   const handleClick = (evt: React.MouseEvent): void => {
   }
@@ -41,10 +43,42 @@ function App(): React.ReactElement {
           zIndex="9999"
           {...CenteredProps}
         >
-          <Space />
-          <RushingClouds />
-          <RocketWrapper />
+          {/* <Space /> */}
+          {/* <RushingClouds /> */}
+          {/* <RocketWrapper /> */}
           <SpaceShuttle />
+          {/* MENU */}
+          <Flex
+            id='menu'
+            // as={"section"}
+            position={"absolute"}
+            bg={"whitesmoke"}
+            width={"30vw"}
+            height={"80%"}
+            right={"10%"}
+            bottom={"17%"}
+            zIndex={"999999"}
+            borderRadius={"15px"}
+          >
+          </Flex>
+          <Tag
+                size='lg'
+                borderRadius='15px'
+                variant='solid'
+                style={{ position: 'absolute', bottom: '11%', right: '10%', zIndex: 9999 }}
+            >
+                <Flex
+                    as="div"
+                    w="20px"
+                    height="20px"
+                    background="green"
+                    marginRight="10px"
+                    borderRadius="full"
+                >
+                </Flex>
+                Waiting for command
+            </Tag>
+          {/* MENU */}
         </Flex>
       </Flex>
     </Flex>
--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,8 +1,9 @@
-import { Flex, Tag } from "@chakra-ui/react";
+import { Flex, Tag, useDisclosure } from "@chakra-ui/react";
 import { CenteredProps } from "../styles/chakra/Props.tsx";
 import React from "react";
 
 export default function Ground() {
+    
     return (
         <Flex
             id='ground'
@@ -13,24 +14,6 @@ export default function Ground() {
             border="8px dashed black"
             {...CenteredProps}
         >
-            <Tag
-                size='lg'
-                borderRadius='full'
-                variant='solid'
-                style={{ position: 'absolute', bottom: '11%', right: '2.5%', zIndex: 9999 }}
-            >
-                <Flex
-                    as="div"
-                    w="20px"
-                    height="20px"
-                    background="green"
-                    marginRight="10px"
-                    borderRadius="full"
-                >
-
-                </Flex>
-                Waiting for command
-            </Tag>
             <Flex
                 id='rocketShadow'
                 {...CenteredProps}
